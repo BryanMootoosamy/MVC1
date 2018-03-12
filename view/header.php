cbe13b23d8efdcac2286037ce24bdfdf03956731
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-03-12 10:57:02 +0100

    html project

---
+++ view/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../assets/style/style.css">
+        <title>Magic Encyclopedia</title>
+    </head>
+    <body>
+        <section class="top">
+            <div class="top1">
+                <img class="banner" src="" alt="Banner">
+                <h1 class="title">Magic Encyclopedia</h1>
+            </div>
+            <div class="top2">
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+            </div>
+        </section>
+    </body>
+</html>
